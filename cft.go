0a0d3faadebd7e2ab6f88a6419b1ddc739421826
tjinjin <tjinjinprogram@gmail.com>
2016-11-17 01:16:27 +0900

    サンプルの出力した

--- cft.go
+++ cft.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -11,17 +12,17 @@ func worddayIs(date time.Time) {
 
 func main() {
 	startTime := time.Date(2016, 11, 01, 0, 0, 0, 0, time.Local)
-	currentTime := time.Date(2016, 11, 10, 0, 0, 0, 0, time.Local)
+	currentTime := time.Now()
 	duration := currentTime.Sub(startTime)
 	fmt.Println(startTime)
 	fmt.Println(currentTime)
 	fmt.Printf("%T\n", currentTime)
 	fmt.Println(duration)
-	fmt.Println(duration.Hours() / 24)
-	output()
+	fmt.Println(math.Ceil(duration.Hours() / 24))
+	output(int(math.Ceil(duration.Hours()/24)) * 8)
 }
 
-func output() {
-	fmt.Println("今月の規定flextimeはXXです")
-	fmt.Println("今日時点での規定flextimeはXXです")
+func output(i int) {
+	fmt.Printf("今月の規定flextimeは%d 時間です\n", 1000)
+	fmt.Printf("今日時点の規定flextimeは%d 時間です\n", i)
 }
